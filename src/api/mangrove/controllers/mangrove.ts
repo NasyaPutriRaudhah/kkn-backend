7d73704c1762bf2fed4aec8dbef41652180f93a8
nasya.putri.raudhah.dahlan@gmail.com <nasya.putri.raudhah.dahlan@gmail.com>
2026-05-29 19:39:55 +0700

    add mangrove section

---
+++ src/api/mangrove/controllers/mangrove.ts
@@ -0,0 +1,7 @@
+﻿/**
+ * mangrove controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::mangrove.mangrove');
---
+++ src/api/mangrove/routes/mangrove.ts
@@ -0,0 +1,7 @@
+﻿/**
+ * mangrove router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::mangrove.mangrove');
