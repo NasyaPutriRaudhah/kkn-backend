0d2423fc5b63e658bf2b4c8a6980996b0be086b5
nasya.putri.raudhah.dahlan@gmail.com <nasya.putri.raudhah.dahlan@gmail.com>
2026-05-29 08:38:57 +0700

    add changes

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -11,7 +11,7 @@ const config: Core.Config.Middlewares = [
       origin: [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
-        'https://kkn-frontend-alpha.vercel.app/',
+        'https://kkn-frontend-alpha.vercel.app',
       ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
